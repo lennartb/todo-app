7911ad3021080a843ee14ab6a387bfaa39804031
Jan Grape <jan.grape@crisp.se>
2015-12-14 23:11:48 +0100

    Added @Service annotation to mongo repo

--- src/main/java/todo/TodoItemRepository.java
+++ src/main/java/todo/TodoItemRepository.java
@@ -1,5 +1,7 @@
 package todo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface TodoItemRepository extends MongoRepository<TodoItem, String> {}
