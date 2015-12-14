f33e7f5780d176473e9b262b706310948d078d7f
Jan Grape <jan.grape@crisp.se>
2015-12-14 23:26:54 +0100

    Using Lombok to auto generate getters/setters & toSting

--- src/main/java/todo/TodoItem.java
+++ src/main/java/todo/TodoItem.java
@@ -1,40 +1,15 @@
 package todo;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+@Data
 public class TodoItem {
-
   @Id private String id;
   private boolean done = false;
-  private String what;
-
-  public TodoItem(String what) {
-    this.done = false;
-    this.what = what;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public boolean isDone() {
-    return done;
-  }
-
-  public String getWhat() {
-    return what;
-  }
+  private final String what;
 
   public void complete() {
     this.done = true;
-  }
-
-  @Override
-  public String toString() {
-    return "TodoItem{" +
-        "done=" + done +
-        ", id='" + id + '\'' +
-        ", what='" + what + '\'' +
-        '}';
   }
 }
